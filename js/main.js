2490b44e2215874032d122f3e0dc7ed45fc6eeec
Ivan Sokolenko <ivan.sokolenko.2017@gmail.com>
2024-12-14 15:27:13 +0200

    выполнен урок №2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Project Title";
+let screens = "Простые,Сложные,Интерактивные";
+let screenPrice = 1000;
+let rollback = 5;
+let fullPrice = 20000;
+let adaptive = true;
 
-alert("Привет!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-console.log("Привет!!");
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + "рублей");
+console.log("Стоимость разработки сайта " + fullPrice + "рублей");
+
+screens = screens.toLowerCase();
+console.log(screens.split(","));
+
+console.log(
+  "Процент отката посреднику за работу: " + fullPrice * (rollback / 100)
+);
