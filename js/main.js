0739e5b305ea07e697ddfc6258b4704a50bb040d
Ivan Sokolenko <ivan.sokolenko.2017@gmail.com>
2024-12-25 14:53:46 +0200

    Выполнено задание урока №10

--- js/main.js
+++ js/main.js
@@ -1,132 +1,30 @@
 "use strict";
 
-const appData = {
-  title: "",
-  screens: [],
-  screenPrice: 0,
-  rollback: 5,
-  adaptive: true,
-  services: [],
-  servicePercentPrice: 0,
-  allServicePrices: 0,
-  fullPrice: 0,
+const library = document.querySelector(".books");
+const books = document.querySelectorAll(".book");
 
-  isNumber: function (num) {
-    if (num === null) {
-      return 0;
-    }
+library.prepend(books[1]);
+library.append(books[2]);
+books[4].after(books[3]);
 
-    return !isNaN(parseFloat(num)) && isFinite(num);
-  },
+document.body.style.backgroundImage = "url('./image/you-dont-know-js.jpg')";
 
-  isString: function (str) {
-    return typeof str === "string" && /[^\d]/.test(str);
-  },
+books[4].querySelector("a").textContent = "Книга 3. this и Прототипы Объектов";
 
-  asking: function () {
-    do {
-      appData.title = prompt(
-        "Как называется ваш проект?",
-        "Калькулятор верстки"
-      );
-    } while (!appData.isString(appData.title));
+let chapters = books[0].querySelectorAll("li");
+chapters[9].after(chapters[2]);
+chapters[3].after(chapters[6]);
+chapters[6].after(chapters[8]);
 
-    for (let i = 0; i < 2; i++) {
-      let name = "";
-      let price = 0;
-      do {
-        name = prompt("Какие типы экранов нужно разработать?");
-      } while (!appData.isString(name));
+chapters = books[5].querySelectorAll("li");
+chapters[1].after(chapters[9]);
+chapters[9].after(chapters[3]);
+chapters[3].after(chapters[4]);
+chapters[7].after(chapters[5]);
 
-      do {
-        price = prompt("Сколько будет стоить данная работа?");
-      } while (!appData.isNumber(price));
-
-      appData.screens.push({ id: i, name: name, price: price });
-    }
-
-    appData.screenPrice = Number(appData.screenPrice);
-
-    appData.adaptive = confirm("Нужен ли адаптив на сайте?");
-
-    for (let i = 0; i < 2; i++) {
-      let price;
-      let name = "";
-      do {
-        name = prompt("Какой допольнительный тип услуги нужен?");
-      } while (!appData.isString(name));
-
-      do {
-        price = prompt("Сколько это будет стоить?");
-      } while (!appData.isNumber(price));
-
-      appData.services.push({ id: i, name: name, price: price });
-    }
-  },
-
-  addPrices: function () {
-    appData.screenPrice = appData.screens.reduce(
-      (sum, screen) => (sum += +screen.price),
-      0
-    );
-
-    appData.allServicePrices = appData.services.reduce(
-      (sum, service) => (sum += +service.price),
-      0
-    );
-  },
-
-  getRollBackMessage: function (price) {
-    if (price >= 30000) {
-      return "Даем скидку в 10%";
-    } else if (price >= 15000 && price < 30000) {
-      return "Даем скидку в 5%";
-    } else if (price < 15000 && price > 0) {
-      return "Скидка не предусмотрена";
-    } else if (price <= 0) {
-      return "Что то пошло не так";
-    }
-  },
-
-  getFullPrice: function (fullPrice, allServicePrices) {
-    appData.fullPrice = fullPrice + allServicePrices;
-  },
-
-  getTitle: function (title) {
-    title = title.trim();
-    appData.title = title[0].toUpperCase() + title.slice(1).toLowerCase();
-  },
-
-  getServicePercentPrice: function (fullPrice, rollback) {
-    appData.servicePercentPrice = Math.ceil(
-      fullPrice - fullPrice * (rollback / 100)
-    );
-  },
-
-  start: function () {
-    appData.asking();
-
-    appData.addPrices();
-
-    appData.getFullPrice(appData.screenPrice, appData.allServicePrices);
-
-    appData.getTitle(appData.title);
-
-    appData.getServicePercentPrice(appData.fullPrice, appData.rollback);
-
-    appData.logger();
-  },
-
-  logger: function () {
-    console.log("allServicePrices", appData.allServicePrices);
-    console.log(appData.screens);
-    console.log(appData.getRollBackMessage(appData.fullPrice));
-    console.log(
-      "Стоимость работы за вычетом процента отката посреднику: " +
-        appData.servicePercentPrice +
-        " рублей"
-    );
-  },
-};
-
-appData.start();
+books[2]
+  .querySelector("ul")
+  .lastElementChild.insertAdjacentHTML(
+    "beforebegin",
+    "<li>Глава 8: За пределами ES6</li>"
+  );
